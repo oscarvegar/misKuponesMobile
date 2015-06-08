6a76e669958f8e78bdcb10a32e2b77a72922d783
oscar.vega <oscar.vega@atomicware.mx>
2015-06-08 12:35:57 -0500

    cambios pal TOKA

--- www/js/login.js
+++ www/js/login.js
@@ -51,7 +51,7 @@ angular.module('starter', ['ionic', 'starter.controllers','validation.match', 'k
     });
   }
 
-  $scope.registrar = function(){
+  $scope.registrar = function(){ 
     $ionicLoading.show({
       template: ' Registrando nuevo usuario ...'
     });
